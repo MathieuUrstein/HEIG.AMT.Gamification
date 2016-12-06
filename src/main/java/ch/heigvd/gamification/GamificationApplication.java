4be12ff2c78309595d10c3bf4102ac35c7b3165d
Basile Vu <basile.vu@gmail.com>
2016-12-06 14:45:57 +0100

    Base structure

---
+++ src/main/java/ch/heigvd/gamification/GamificationApplication.java
@@ -0,0 +1,12 @@
+package ch.heigvd.gamification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamificationApplication.class, args);
+	}
+}
