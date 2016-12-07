9fb634938f1a448b5a858fd0b79788069e970997
sebastien_boson_heigvd <sebastien.boson@heig-vd.ch>
2016-12-07 09:59:22 +0100

    add endpoints

--- src/main/java/ch/heigvd/gamification/web/PointScaleRestController.java
+++ src/main/java/ch/heigvd/gamification/web/PointScaleRestController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by sebbos on 06.12.2016.
+ * Created by sebbos on 07.12.2016.
  */
 @RestController
-@RequestMapping("/badges")
-class BadgeRestController {
+@RequestMapping("/pointScales")
+public class PointScaleRestController {
 
     @RequestMapping(method = RequestMethod.GET)
     String read() {
