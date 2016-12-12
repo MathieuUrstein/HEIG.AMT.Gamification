0759c0537e70b5fb966a0aea406c555a958ce52e
sebastien_boson_heigvd <sebastien.boson@heig-vd.ch>
2016-12-12 14:09:00 +0100

    starting adding validator

---
+++ src/main/java/ch/heigvd/gamification/dto/BadgeDTO.java
@@ -0,0 +1,33 @@
+package ch.heigvd.gamification.dto;
+
+/**
+ * Created by sebbos on 12.12.2016.
+ */
+public class BadgeDTO {
+    private String name;
+    private byte[] image;
+
+    public BadgeDTO() {
+    }
+
+    public BadgeDTO(String name, byte[] image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+}
