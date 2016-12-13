ec3ac85b9005aee9867c93aea47ca911076ba22f
Basile Vu <basile.vu@gmail.com>
2016-12-13 11:56:58 +0100

    Adding ApplicationRepository

---
+++ src/main/java/ch/heigvd/gamification/dao/ApplicationRepository.java
@@ -0,0 +1,8 @@
+package ch.heigvd.gamification.dao;
+
+import ch.heigvd.gamification.model.Application;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ApplicationRepository extends CrudRepository<Application, Long> {
+    public Application findByName(String name);
+}
--- src/main/java/ch/heigvd/gamification/web/AuthenticationRestController.java
+++ src/main/java/ch/heigvd/gamification/web/AuthenticationRestController.java
@@ -1,7 +1,9 @@
 package ch.heigvd.gamification.web;
 
 
+import ch.heigvd.gamification.dao.ApplicationRepository;
 import ch.heigvd.gamification.dto.CredentialsDTO;
+import ch.heigvd.gamification.model.Application;
 import ch.heigvd.gamification.util.AuthenticationUtils;
 import ch.heigvd.gamification.util.JWTUtils;
 import ch.heigvd.gamification.util.URIs;
@@ -14,11 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(URIs.AUTH)
 public class AuthenticationRestController {
-    
+    private final ApplicationRepository applicationRepository;
+
+    public AuthenticationRestController(ApplicationRepository applicationRepository) {
+        this.applicationRepository = applicationRepository;
+    }
+
     public ResponseEntity login(@RequestBody CredentialsDTO credentials) {
 
+        Application app = applicationRepository.findByName(credentials.getApplicationName());
+
         // TODO request app from DB
-        if (!AuthenticationUtils.isPasswordValid(credentials.getPassword(), "")) {
+        if (!AuthenticationUtils.isPasswordValid(credentials.getPassword(), app.getPassword())) {
             return ResponseEntity
                     .status(HttpStatus.UNAUTHORIZED)
                     .build();
