1df2af3a2e507683203ad12de1ad326bafb7f58e
Basile Vu <basile.vu@gmail.com>
2016-12-13 13:45:24 +0100

    ExceptionHandler for AuthenticationRestController

---
+++ src/main/java/ch/heigvd/gamification/validator/CredentialsDTOValidator.java
@@ -0,0 +1,20 @@
+package ch.heigvd.gamification.validator;
+
+import ch.heigvd.gamification.dto.BadgeDTO;
+import ch.heigvd.gamification.dto.CredentialsDTO;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class CredentialsDTOValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return CredentialsDTO.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        ValidationUtils.rejectIfEmpty(errors, "applicationName", "42"); // FIXME error code
+        ValidationUtils.rejectIfEmpty(errors, "password", "42"); // FIXME error code
+    }
+}
--- src/main/java/ch/heigvd/gamification/web/AuthenticationRestController.java
+++ src/main/java/ch/heigvd/gamification/web/AuthenticationRestController.java
@@ -7,27 +7,44 @@ import ch.heigvd.gamification.model.Application;
 import ch.heigvd.gamification.util.AuthenticationUtils;
 import ch.heigvd.gamification.util.JWTUtils;
 import ch.heigvd.gamification.util.URIs;
+import ch.heigvd.gamification.validator.CredentialsDTOValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(URIs.AUTH)
 public class AuthenticationRestController {
     private final ApplicationRepository applicationRepository;
 
+    @Autowired
     public AuthenticationRestController(ApplicationRepository applicationRepository) {
         this.applicationRepository = applicationRepository;
     }
 
-    public ResponseEntity login(@RequestBody CredentialsDTO credentials) {
+    @InitBinder
+    void initBinder(WebDataBinder binder) {
+        binder.setValidator(new CredentialsDTOValidator());
+    }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody List<FieldError> processValidationError(MethodArgumentNotValidException ex) {
+        return ex.getBindingResult().getFieldErrors(); // FIXME return custom Error
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity login(@Valid @RequestBody CredentialsDTO credentials) {
         Application app = applicationRepository.findByName(credentials.getApplicationName());
 
-        // TODO request app from DB
-        if (!AuthenticationUtils.isPasswordValid(credentials.getPassword(), app.getPassword())) {
+        if (app == null || !AuthenticationUtils.isPasswordValid(credentials.getPassword(), app.getPassword())) {
             return ResponseEntity
                     .status(HttpStatus.UNAUTHORIZED)
                     .build();
