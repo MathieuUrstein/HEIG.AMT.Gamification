be3168056e781e95e5e671e7e02053b7a062749b
sebastien_boson_heigvd <sebastien.boson@heig-vd.ch>
2016-12-14 01:03:48 +0100

    add custom info in a response for custom exception

--- src/main/java/ch/heigvd/gamification/web/controller/GlobalControllerExceptionHandler.java
+++ src/main/java/ch/heigvd/gamification/web/controller/GlobalControllerExceptionHandler.java
@@ -1,6 +1,9 @@
 package ch.heigvd.gamification.web.controller;
 
 import ch.heigvd.gamification.error.ErrorBadRequest;
+import org.springframework.boot.autoconfigure.web.DefaultErrorAttributes;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -9,8 +12,11 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.RequestAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by sebbos on 13.12.2016.
@@ -29,5 +35,22 @@ public class GlobalControllerExceptionHandler {
         }
 
         return errorBadRequest;
+    }
+
+    @Bean
+    public ErrorAttributes errorAttributes() {
+        return new DefaultErrorAttributes() {
+            @Override
+            public Map<String, Object> getErrorAttributes(RequestAttributes requestAttributes, boolean includeStackTrace) {
+                Map<String, Object> errorAttributes = super.getErrorAttributes(requestAttributes, false);
+                Map<String, Object> error = new HashMap<>();
+
+                // We don't need other unusable info
+                error.put("path", errorAttributes.get("path"));
+                error.put("message", errorAttributes.get("message"));
+
+                return error;
+            }
+        };
     }
 }
