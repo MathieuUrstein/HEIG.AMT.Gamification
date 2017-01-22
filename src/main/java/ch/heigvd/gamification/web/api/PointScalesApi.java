58116b387e3e0a6b674d3b805412127a445a8731
sebastien_boson_heigvd <sebastien.boson@heig-vd.ch>
2017-01-22 18:07:17 +0100

    add put for /pointScales endpoint with api-spec update

--- src/main/java/ch/heigvd/gamification/web/api/PointScalesApi.java
+++ src/main/java/ch/heigvd/gamification/web/api/PointScalesApi.java
@@ -6,10 +6,11 @@ import ch.heigvd.gamification.model.PointScale;
 import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.annotations.ApiIgnore;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Api(value = "pointScales", description = "the pointScales API")
@@ -89,38 +90,26 @@ public interface PointScalesApi {
                                           @ApiParam(value = "The info needed to create a point scale.", required = true)
                                           @RequestBody PointScaleDTO pointScaleDTO);
 
-
-    // TODO
-    /*
-    @ApiOperation(value = "Partially updates the given point scale.", notes = "", response = Void.class,
+    @ApiOperation(value = "Makes a complete update for a given badge.",
+            notes = "",
+            response = Void.class,
             authorizations = {
                     @Authorization(value = "JWT")
-            }, tags = {})
+            }, tags={  })
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successful operation.", response = Void.class),
-            @ApiResponse(code = 404, message = "Point scale not found.", response = Void.class),
-            @ApiResponse(code = 409, message = "Error code 3: Point scale name must be unique in the current " +
-                    "application.", response = Void.class)})
-    ResponseEntity<Void> pointScalesIdPatch(@ApiParam(value = "The id of the point scale.", required = true)
-                                            @PathVariable("id") BigDecimal id,
-                                            @ApiParam(value = "The new info of the point scale.") @RequestBody
-                                                    PointScale body);
-
-
-
-    @ApiOperation(value = "Updates the given point scale.", notes = "", response = Void.class, authorizations = {
-            @Authorization(value = "JWT")
-    }, tags = {})
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successful operation.", response = Void.class),
-            @ApiResponse(code = 404, message = "Point scale not found.", response = Void.class),
-            @ApiResponse(code = 409, message = "Error code 3: Point scale name must be unique in the current " +
-                    "application.", response = Void.class)})
-    ResponseEntity<Void> pointScalesIdPut(@ApiParam(value = "The id of the point scale.", required = true)
-                                          @PathVariable("id") BigDecimal id,
-                                          @ApiParam(value = "The new info of the point scale.", required = true)
-                                          @RequestBody PointScale body);
-    */
+            @ApiResponse(code = 200,
+                    message = "Successful operation.",
+                    response = Void.class),
+            @ApiResponse(code = 404,
+                    message = "Point scale not found.",
+                    response = Void.class),
+            @ApiResponse(code = 409,
+                    message = "Error code 3: Point scale name must be unique in the current application.",
+                    response = Void.class)
+    })
+    ResponseEntity<Void> completeUpdatePointScale(@ApiIgnore @RequestAttribute("application") Application application,
+                                                  @ApiParam(value = "The name of the point scale.", required = true) @PathVariable("name") String name,
+                                                  @ApiParam(value = "The new info of the point scale.", required=true) @Valid @RequestBody PointScaleDTO pointScaleDTO);
 
     @ApiOperation(
             value = "Deletes the given point scale.",
